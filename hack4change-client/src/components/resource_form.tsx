97a1a1263fe12e96bbfe73227aefa72ebc91e1f3
Justin <gaudet@gmail.com>
2026-03-15 07:35:44 -0300

    client - resource form

--- hack4change-client/src/components/resource_form.tsx
+++ hack4change-client/src/components/resource_form.tsx
@@ -1,12 +1,10 @@
 import { useState } from "react"
-import "./App.css"
+import { useLocation }  from "react-router-dom"
+import "../App.css"
+import Navbar from "./navbar"
 
-interface Props {
-  organization: string
-}
-
-export default function DonationForm({ organization }: Props) {
-
+export default function ResourceForm() {
+  const { state } = useLocation()
   const [itemName, setItemName] = useState("")
   const [description, setDescription] = useState("")
   const [quantity, setQuantity] = useState("")
@@ -16,21 +14,18 @@ export default function DonationForm({ organization }: Props) {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
-
     setSubmitted(true)
   }
 
   return (
     <div>
 
-      <div className="navbar">
-        <div className="logo">HopeBridge</div>
-      </div>
+      <Navbar />
 
       <div className="form-container">
 
         <div className="form-title">
-          Donation to <strong>{organization}</strong>
+          { state.form_type == 'donate' ? "Donation" : "Request" }
         </div>
 
         <form onSubmit={handleSubmit}>
@@ -77,8 +72,8 @@ export default function DonationForm({ organization }: Props) {
             onChange={(e) => setExpirationDate(e.target.value)}
           />
 
-          <button className="submit-btn">
-            Finalize Donation
+          <button className="submit-btn" type="submit">
+            Finalize { state.form_type == 'donate' ? "Donation" : "Request" }
           </button>
 
         </form>
@@ -89,11 +84,11 @@ export default function DonationForm({ organization }: Props) {
         <div className="popup">
 
           <div className="popup-box">
-
-            <h3>Donation Confirmed 🎉</h3>
-
             <p>
-              Your donation has been sent to <strong>{organization}</strong>.
+            { state.form_type == 'donate' ? 
+              "We've posted the availability of your generous donation. An organization will let you know as soon as they are in need." :
+              "We've posted your request. One of our generous doners will let you know as soon as something is available that meets your needs."
+            }
             </p>
 
             <button onClick={() => setSubmitted(false)}>
